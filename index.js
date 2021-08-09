7a41667b3b8b8ab5a15df8cdf177b4098bd480b2
Lydia <lreitzel@gmail.com>
2021-08-09 17:36:20 -0400

    commit tree 7808b733529c0880de9a92b8bce3f45e2490a10f

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize (string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' );
+  }
+
+  static titleize (string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let answer = []
+    let arr = string.split(" ")
+    for (let i = 0; i < arr.length; i++) {
+      if (i == 0) {
+        answer.push(this.capitalize(arr[i]))
+      } else {
+        if (exceptions.includes(arr[i])) {
+          answer.push(arr[i])
+        } else {
+          answer.push(this.capitalize(arr[i]))
+        }
+      }
+    }
+    return answer.join(" ")
+  }
 }
